3d3c13d718c1ac6cfacee8eef5313bc3e722a18f
Kenneth Watson <ken@ecn.co.za>
2019-02-26 20:43:19 +0200

    add test

--- src/net/shrimpworks/unreal/submitter/Main.java
+++ src/net/shrimpworks/unreal/submitter/Main.java
@@ -2,7 +2,7 @@ package net.shrimpworks.unreal.submitter;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
 }
---
+++ test/net/shrimpworks/unreal/submitter/NopTest.java
@@ -0,0 +1,13 @@
+package net.shrimpworks.unreal.submitter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class NopTest {
+
+	@Test
+	public void nop() {
+		fail("TODO");
+	}
+}
