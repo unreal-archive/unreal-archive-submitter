b684c9f0c8ba9f91a92146c568026e5ae37c802e
Kenneth Watson <ken@ecn.co.za>
2019-04-15 20:13:37 +0200

    dispatch exchanges off io threads

--- src/net/shrimpworks/unreal/submitter/WebApp.java
+++ src/net/shrimpworks/unreal/submitter/WebApp.java
@@ -16,6 +16,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.timgroup.statsd.StatsDClient;
 import io.undertow.Handlers;
@@ -32,6 +33,7 @@ import io.undertow.util.Headers;
 import io.undertow.util.HttpString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xnio.Options;
 
 import net.shrimpworks.unreal.archive.ArchiveUtil;
 import net.shrimpworks.unreal.archive.Util;
@@ -39,6 +41,9 @@ import net.shrimpworks.unreal.archive.Util;
 public class WebApp implements Closeable {
 
 	private static final Logger logger = LoggerFactory.getLogger(WebApp.class);
+
+	private static final int WORKER_IO_THREADS = 2;
+	private static final int WORKER_TASK_CORE_THREADS = 10;
 
 	private static final String HTTP_ROOT = "/";
 	private static final String HTTP_UPLOAD = "/upload";
@@ -70,6 +75,13 @@ public class WebApp implements Closeable {
 										 .add("GET", HTTP_JOB, jobHandler());
 
 		this.server = Undertow.builder()
+							  .setWorkerOption(Options.WORKER_IO_THREADS, WORKER_IO_THREADS)
+							  .setWorkerOption(Options.WORKER_TASK_CORE_THREADS, WORKER_TASK_CORE_THREADS)
+							  .setWorkerOption(Options.WORKER_TASK_MAX_THREADS, WORKER_TASK_CORE_THREADS)
+							  .setWorkerOption(Options.TCP_NODELAY, true)
+							  .setSocketOption(Options.WORKER_IO_THREADS, WORKER_IO_THREADS)
+							  .setSocketOption(Options.TCP_NODELAY, true)
+							  .setSocketOption(Options.REUSE_ADDRESSES, true)
 							  .addHttpListener(bindAddress.getPort(), bindAddress.getHostString())
 							  .setHandler(handler)
 							  .build();
@@ -109,49 +121,54 @@ public class WebApp implements Closeable {
 		HttpHandler multipartProcessorHandler = (exchange) -> {
 			statsD.count("www.upload", 1);
 			final long start = System.currentTimeMillis();
-			try {
-				Submissions.Job job = new Submissions.Job();
-				jobs.put(job.id, job);
+			exchange.dispatch(() -> {
+				try {
+					Submissions.Job job = new Submissions.Job();
+					jobs.put(job.id, job);
 
-				FormData attachment = exchange.getAttachment(FormDataParser.FORM_DATA);
-				final List<Path> files = attachment.get("files").stream().map(v -> {
-					try {
-						Path file = v.getFileItem().getFile();
-						String newName = String.format("%s_%s.%s",
-													   Util.plainName(v.getFileName()),
-													   Util.hash(file).substring(0, 8),
-													   Util.extension(v.getFileName()));
+					FormData attachment = exchange.getAttachment(FormDataParser.FORM_DATA);
+					final List<Path> files = attachment.get("files").stream().map(v -> {
+						try {
+							Path file = v.getFileItem().getFile();
+							String newName = String.format("%s_%s.%s",
+														   Util.plainName(v.getFileName()),
+														   Util.hash(file).substring(0, 8),
+														   Util.extension(v.getFileName()));
 
-						return Files.move(file, tmpDir.resolve(newName), StandardCopyOption.REPLACE_EXISTING);
-					} catch (IOException e) {
-						job.log(Submissions.JobState.FAILED, String.format("Failed moving file %s", v.getFileName()), e);
-						statsD.count("www.upload.fileFail", 1);
-						logger.error("File move failed", e);
-						return null;
+							return Files.move(file, tmpDir.resolve(newName), StandardCopyOption.REPLACE_EXISTING);
+						} catch (IOException e) {
+							job.log(Submissions.JobState.FAILED, String.format("Failed moving file %s", v.getFileName()), e);
+							statsD.count("www.upload.fileFail", 1);
+							logger.error("File move failed", e);
+							return null;
+						}
+					}).filter(Objects::nonNull).collect(Collectors.toList());
+
+					if (!files.isEmpty()) {
+						job.log(String.format("Received file(s): %s, queue for processing",
+											  files.stream().map(Util::fileName).collect(Collectors.joining(", "))));
+
+						subProcessor.add(new SubmissionProcessor.PendingSubmission(
+								job, System.currentTimeMillis(), Util.fileName(files.get(0)), files.toArray(PATH_ARRAY)
+						));
+
+						statsD.count("www.upload.fileAdd", files.size());
 					}
-				}).filter(Objects::nonNull).collect(Collectors.toList());
 
-				if (!files.isEmpty()) {
-					job.log(String.format("Received file(s): %s, queue for processing",
-										  files.stream().map(Util::fileName).collect(Collectors.joining(", "))));
+					exchange.getResponseHeaders()
+							.put(Headers.CONTENT_TYPE, "application/json")
+							.put(new HttpString("Access-Control-Allow-Origin"), allowOrigins)
+							.put(new HttpString("Access-Control-Allow-Methods"), "POST");
+					exchange.getResponseSender().send(MAPPER.writeValueAsString(job.id));
 
-					subProcessor.add(new SubmissionProcessor.PendingSubmission(
-							job, System.currentTimeMillis(), Util.fileName(files.get(0)), files.toArray(PATH_ARRAY)
-					));
-
-					statsD.count("www.upload.fileAdd", files.size());
+					statsD.count("www.upload.ok", 1);
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				} finally {
+					statsD.time("www.upload", System.currentTimeMillis() - start);
+					exchange.endExchange();
 				}
-
-				exchange.getResponseHeaders()
-						.put(Headers.CONTENT_TYPE, "application/json")
-						.put(new HttpString("Access-Control-Allow-Origin"), allowOrigins)
-						.put(new HttpString("Access-Control-Allow-Methods"), "POST");
-				exchange.getResponseSender().send(MAPPER.writeValueAsString(job.id));
-
-				statsD.count("www.upload.ok", 1);
-			} finally {
-				statsD.time("www.upload", System.currentTimeMillis() - start);
-			}
+			});
 		};
 
 		return new EagerFormParsingHandler(
@@ -174,18 +191,27 @@ public class WebApp implements Closeable {
 					.put(new HttpString("Access-Control-Allow-Origin"), allowOrigins)
 					.put(new HttpString("Access-Control-Allow-Methods"), "POST, GET");
 
-			if (job == null) {
-				exchange.setStatusCode(404);
-				exchange.getResponseSender().send("[]");
-				return;
-			}
+			exchange.dispatch(() -> {
+				try {
+					if (job == null) {
+						exchange.setStatusCode(404);
+						exchange.getResponseSender().send("[]");
+						exchange.endExchange();
+						return;
+					}
 
-			final Deque<String> catchup = exchange.getQueryParameters().getOrDefault("catchup", emptyDeque);
-			if (!catchup.isEmpty() && catchup.getFirst().equals("1")) {
-				exchange.getResponseSender().send(MAPPER.writeValueAsString(job.log));
-			} else {
-				exchange.getResponseSender().send(MAPPER.writeValueAsString(job.pollLog(Duration.ofSeconds(15))));
-			}
+					final Deque<String> catchup = exchange.getQueryParameters().getOrDefault("catchup", emptyDeque);
+					if (!catchup.isEmpty() && catchup.getFirst().equals("1")) {
+						exchange.getResponseSender().send(MAPPER.writeValueAsString(job.log));
+					} else {
+						exchange.getResponseSender().send(MAPPER.writeValueAsString(job.pollLog(Duration.ofSeconds(15))));
+					}
+				} catch (JsonProcessingException e) {
+					throw new RuntimeException(e);
+				} finally {
+					exchange.endExchange();
+				}
+			});
 		};
 	}
 
